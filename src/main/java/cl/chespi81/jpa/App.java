6b5b8802eb4abd2da0ab08ac1410105baf083128
Roberto San Martin <rsanmartin@tinet.cl>
2018-02-28 01:12:59 -0300

    Versión inicial del proyecto (creado desde arquetipo básico macen).

---
+++ src/main/java/cl/chespi81/jpa/App.java
@@ -0,0 +1,13 @@
+package cl.chespi81.jpa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
